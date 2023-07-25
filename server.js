cd9658d669f95bae338566ef3a42cd248f41aeae
HiteshAttri <hiteshattri71@gmail.com>
2023-07-25 18:45:12 +0530

    msg saving on mongodb cloud, checkpoint 2

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express();
-const config = require('./config');
+const config = require('./config/config');
 
 const port = 5000;
 
-
 app.use(express.json()); 
 app.use(express.urlencoded({ extended:true }));
+
+const contactMeRoute = require('./routes/saveMsg');
+
+app.get('/testing',(req,res)=>{
+    res.json({msg:"tesing node server and mongodb connection"})
+})
+
+app.use("/contact-me", contactMeRoute)
+
+app.listen(port,(error)=>{
+    // if(!error) console.log("main--> Server running at port,", port);
+    if(!error) console.log(`App listening at http://localhost:${port}`)
+    else console.log("Error! ", error);
+})
